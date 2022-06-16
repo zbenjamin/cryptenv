eea21d8e0e29b4c76bb85224e5415bad16b78be1
Zev Benjamin <zev@strangersgate.com>
2022-06-15 18:04:16 -0700

    Add support for unmounting and shell subcommand

--- cryptenv.py
+++ cryptenv.py
@@ -49,13 +49,49 @@ def mount(filename):
                                       filename],
                                      input=format_hdiutil_passphrase_stdin(passphrase))
     structured_out = plistlib.loads(output)
-    mountpoint = None
+    mount_point = None
     for elem in structured_out['system-entities']:
         if 'mount-point' in elem:
-            mountpoint = elem['mount-point']
+            mount_point = elem['mount-point']
             break
 
-    click.echo("Mounted encrypted environment at {}".format(mountpoint))
+    click.echo("Mounted encrypted environment at {}".format(mount_point))
+
+    return mount_point
+
+def find_disk(mount_point):
+    output = subprocess.check_output(["hdiutil", "info", "-plist"])
+    structured_out = plistlib.loads(output)
+    disk = None
+    for images in structured_out['images']:
+        for image in images['system-entities']:
+            if 'mount-point' in image and image['mount-point'] == mount_point:
+                disk = image['dev-entry']
+                break
+
+    return disk
+
+def do_unmount(disk):
+    output = subprocess.check_call(["hdiutil", "detach", disk])
+
+@cli.command()
+@click.argument("mount_point", type=click.Path(exists=True, dir_okay=True,
+                                               file_okay=False, allow_dash=False))
+def unmount(mount_point):
+    """Unmount an encrypted environment"""
+    disk = find_disk(mount_point)
+    do_unmount(disk)
+
+@cli.command()
+@click.argument("filename", type=click.Path(exists=True, dir_okay=False,
+                                            readable=True, allow_dash=False))
+@click.pass_context
+def shell(ctx, filename):
+    """Mount an encrypted environment and start a shell in it. Unmount the environment when the shell exits"""
+    mount_point = ctx.forward(mount)
+    # XXX: don't assume the mount point
+    subprocess.call(["/bin/bash", "--rcfile", "/Volumes/cryptenv/bashrc", "-i"])
+    ctx.invoke(unmount, mount_point=mount_point)
 
 if __name__ == '__main__':
     cli()
